af71d6462c17358aa8716082f36a4e8c18234dfd
Lizzie2021 <liguanna2017@outlook.com>
2022-09-14 15:20:46 +1000

    fix validation

--- index.js
+++ index.js
@@ -16,121 +16,167 @@ app.get("/", (req, res) => {
 });
 const userSchema = new mongoose.Schema({
   username: String,
-  description: String,
-  duration: Number,
-  date: String,
   from: String,
   to: String,
   count: Number,
-  log: [Object],
+  log: [
+    {
+      description: String,
+      duration: Number,
+      date: String,
+    },
+  ],
 });
 const User = mongoose.model("User", userSchema);
 
-app.post("/api/users", urlencodedParser, function (req, res) {
+app.post("/api/users", urlencodedParser, function (req, res, next) {
   let uname = req.body.username;
 
   let user = new User({
     username: uname,
   });
-  user.save((err, data) => {
-    if (err) console.err(err);
-    console.log(data);
-    res.json({
-      username: data.username,
-      _id: data._id,
-    });
-  });
+
+  user
+    .save()
+    .then((data) =>
+      res.json({
+        username: data.username,
+        _id: data._id,
+      })
+    )
+    .catch((err) => next(err));
 });
 
 app.get("/api/users", function (req, res) {
-  User.find({}, "_id username", function (err, data) {
-    if (err) console.err(err);
-    res.json(data);
-  });
+  User.find({}, "_id username")
+    .then((data) => res.json(data))
+    .catch((err) => next(err));
 });
 
-app.post("/api/users/:_id/exercises", urlencodedParser, function (req, res) {
-  let id = req.body._id;
-  let description = req.body.description;
-  let duration = req.body.duration;
-  let date_input = req.body.date;
-  let date, date_format;
+app.post(
+  "/api/users/:_id/exercises",
+  urlencodedParser,
+  function (req, res, next) {
+    let id = req.body._id;
+    let description = req.body.description;
+    let duration = Number(req.body.duration);
+    let date_input = req.body.date;
+    let date, date_format;
 
-  if (date_input === "") {
-    date = new Date();
-  } else {
-    date = new Date(date_input);
-  }
-  date_format = date.toDateString();
-
-  let newvalue = {
-    description: description,
-    duration: duration,
-    date: date_format,
-  };
-  User.updateOne({ _id: id }, newvalue, function (err, data) {
-    if (err) console.err(err);
-    else console.log("Successfully updated!");
-  });
-  User.findById(id, function (err, data) {
-    if (err) console.log(err);
-    data.log.push(newvalue);
-    data.count = data.log.length;
-    data.save();
-    console.log(data);
-  });
-  User.find(
-    { _id: id },
-    "_id username date description duration ",
-    function (err, data) {
-      if (err) console.err(err);
-      console.log(data);
-      res.json(data[0]);
+    if (date_input === "") {
+      date = new Date();
+    } else {
+      date = new Date(date_input);
     }
-  );
-});
+    if (date == "Invalid Date") {
+      res.send("Invalid Date!");
+    }
+    if (description == "" || duration == "") {
+      res.send("Description and duration are required!");
+    }
+
+    date_format = date.toDateString();
+
+    let newvalue = {
+      description: description,
+      duration: duration,
+      date: date_format,
+    };
+
+    // User.updateOne({ _id: id }, newvalue)
+    //   .then((data) => console.log("Successfully updated!"))
+    //   .catch((err) => next(err));
+    // User.findById(id, function (err, data) {
+    //   if (err) console.log(err);
+    //   data.log.push(newvalue);
+    //   data.count = data.log.length;
+    //   data.save();
+    //   console.log(data);
+    // });
+    // User.find(
+    //   { _id: id },
+    //   "_id username date description duration ",
+    //   function (err, data) {
+    //     if (err) console.err(err);
+    //     console.log(data);
+    //     res.json(data[0]);
+    //   }
+    // );
+    User.findOne({ _id: id })
+      .then((data) => {
+        // data.description = description;
+        // data.duration = duration;
+        // data.date = date_format;
+        data.log.push(newvalue);
+        data.count = data.log.length;
+        data.save();
+        res.json({
+          username: data.username,
+          description: description,
+          duration: duration,
+          date: date_format,
+          _id: id,
+        });
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
+);
 
 app.get("/api/users/:_id/logs", function (req, res) {
   let id = req.params._id;
+  let from, to, limit;
   if (Object.keys(req.query).length !== 0) {
-    let from = new Date(req.query.from);
-    let to = new Date(req.query.to);
-    let limit = req.query.limit;
-    console.log(from, to, limit);
-    User.findById(id, "_id username from to count log", function (err, data) {
-      if (err) console.log(err);
-      data.from = from.toDateString();
-      data.to = to.toDateString();
+    if (req.query.from) {
+      from = new Date(req.query.from);
+    } else {
+      from = new Date("1970-01-01");
+    }
+    if (req.query.to) {
+      to = new Date(req.query.to);
+    } else {
+      to = new Date();
+    }
+    if (req.query.limit) {
+      limit = req.query.limit;
+    } else {
+      limit = 10000;
+    }
+    const gettime = (str) => {
+      let d = new Date(str);
+      return d.getTime();
+    };
 
-      const gettime = (str) => {
-        let d = new Date(str);
-        return d.getTime();
-      };
-      let result = data.log.filter((item) => {
-        return (
-          gettime(item.date) >= gettime(data.from) &&
-          gettime(item.date) <= gettime(data.to)
-        );
-      });
-      let arr = result.slice(0, limit);
+    User.findById(id, "_id username from to count log")
+      .then((data) => {
+        data.from = from.toDateString();
+        data.to = to.toDateString();
+        let result = data.log.filter((item) => {
+          return (
+            gettime(item.date) >= gettime(data.from) &&
+            gettime(item.date) <= gettime(data.to)
+          );
+        });
+        let arr = result.slice(0, limit);
 
-      data.save();
-      res.json({
-        _id: id,
-        username: data.username,
-        from: data.from,
-        to: data.to,
-        count: arr.length,
-        log: arr,
-      });
-      console.log(data);
-    });
+        data.save();
+        res.json({
+          _id: id,
+          username: data.username,
+          from: data.from,
+          to: data.to,
+          count: arr.length,
+          log: arr,
+        });
+      })
+      .catch((err) => next(err));
   } else {
-    User.findById(id, "_id username  count log", function (err, data) {
-      if (err) console.err(err);
-      res.json(data);
-      console.log(data);
-    });
+    User.findById(id, "_id username  count log")
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => next(err));
   }
 });
 
